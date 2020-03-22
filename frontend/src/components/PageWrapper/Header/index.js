5cd06679fbd63bcd681b411a6fa4c26c5c45ca29
Oana <oanamuntean8@gmail.com>
2020-03-22 19:56:58 +0200

    Add header, footer, hoem from stam acasa

---
+++ frontend/src/components/PageWrapper/Header/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import { Header as TFHeader } from "@code4ro/taskforce-fe-components";
+import { DevelopedBy } from "@code4ro/taskforce-fe-components";
+
+import { ReactComponent as LogoSvg } from "../../assets/diasporahub.svg";
+
+import "./header.scss";
+
+const Logo = () => (
+  <NavLink to="/">
+    <LogoSvg />
+  </NavLink>
+);
+
+const MenuItems = () => (
+  <>
+    <NavLink className="nav-link" to="/despre">
+      Despre
+    </NavLink>
+    <a className="nav-link" href="https://code4.ro/ro/apps/">
+      Ecosistemul Covid-19
+    </a>
+    <a className="nav-link" href="https://code4.ro/ro/doneaza/">
+      Sprijină proiectul
+    </a>
+  </>
+);
+
+const ProfileItems = () => (
+  <>
+    <NavLink to="/">Contul meu</NavLink>
+    <div className="account-separator"></div>
+    <NavLink to="/login">Login</NavLink>
+  </>
+);
+
+const Header = () => {
+  return (
+    <>
+      <TFHeader
+        Logo={<Logo />}
+        MenuItems={<MenuItems />}
+        ProfileItems={<ProfileItems />}
+      />
+      <DevelopedBy />
+    </>
+  );
+};
+
+export default Header;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,8 +1,10 @@
+import Home from "./components/Home";
+
 export const ROUTES = {
   home: {
     path: "/",
     extraProps: { exact: true },
-    component: () => "Placeholder home"
+    component: Home
   },
   login: {
     path: "/login",
