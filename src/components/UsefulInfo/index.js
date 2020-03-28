a8ddefda2e7016dc9bb81411c0fe27085d8053d5
Eliza N <eliza.nitoi@gmail.com>
2020-03-28 17:22:55 +0200

    Extract data in separate file. Changed item styling

--- src/components/UsefulInfo/index.js
+++ src/components/UsefulInfo/index.js
@@ -1,45 +1,25 @@
 import React from "react";
 import { Hero, SearchResultListItem } from "@code4ro/taskforce-fe-components";
+import usefulInfoList from "../../data/useful-info";
+
 import "./styles.scss";
 
 const UsefulInfo = () => {
-  const contentList = [
-    {
-      id: 0,
-      title:
-        "Apel de la Guvernul României. Ce trebuie să facă cetățenii când primesc mesaje pe Facebook și WhatsApp despre coronavirus",
-      date: "12 martie 2020 12:45",
-      readMoreText: "Citeste mai mult",
-      overview:
-        "Guvernul României face apel la cetățenii români, prin intermediul unui clip video postat pe rețelele de socializare, să se informeze despre coronavirus doar din su....."
-    },
-    {
-      id: 1,
-      title:
-        "Apel de la Guvernul României. Ce trebuie să facă cetățenii când primesc mesaje pe Facebook și WhatsApp despre coronavirus",
-      date: "12 martie 2020 12:45",
-      readMoreText: "Citeste mai mult",
-      overview:
-        "Guvernul României face apel la cetățenii români, prin intermediul unui clip video postat pe rețelele de socializare, să se informeze despre coronavirus doar din su....."
-    }
-  ];
-
   return (
     <section className="column is-8">
       <Hero title={"Informații utile pentru românii din diaspora"} />
-      <div className="list-items-grid">
-        {contentList.map(({ id, title, date, overview }) => (
+      {usefulInfoList.map(({ id, title, date, overview }) => (
+        <div key={`useful_info_${id}`} className="useful_info-wrapper">
           <SearchResultListItem
             date={date}
-            key={`useful_info_${id}`}
             title={title}
             readMoreText={"Citeste mai mult"}
             readMoreLink="/"
           >
             <p>{overview} </p>
           </SearchResultListItem>
-        ))}
-      </div>
+        </div>
+      ))}
     </section>
   );
 };
---
+++ src/data/useful-info.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 0,
+    title:
+      "Apel de la Guvernul României. Ce trebuie să facă cetățenii când primesc mesaje pe Facebook și WhatsApp despre coronavirus",
+    date: "12 martie 2020 12:45",
+    readMoreText: "Citeste mai mult",
+    overview:
+      "Guvernul României face apel la cetățenii români, prin intermediul unui clip video postat pe rețelele de socializare, să se informeze despre coronavirus doar din su....."
+  },
+  {
+    id: 1,
+    title:
+      "Apel de la Guvernul României. Ce trebuie să facă cetățenii când primesc mesaje pe Facebook și WhatsApp despre coronavirus",
+    date: "12 martie 2020 12:45",
+    readMoreText: "Citeste mai mult",
+    overview:
+      "Guvernul României face apel la cetățenii români, prin intermediul unui clip video postat pe rețelele de socializare, să se informeze despre coronavirus doar din su....."
+  }
+];
