9f85726a4b9b000e847559ce688538252de159e0
Gauca <gaucab@gb-locs-MacBook-Pro.local>
2020-03-28 11:39:50 +0200

    #14 - updated DevelopedBy to one line

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
   return (
     <>
       <Header Logo={Logo()} MenuItems={MenuItems} />
-      <DevelopedBy showSecondLine={true} />
+      <DevelopedBy />
       <Suspense fallback={<div></div>}>
         <main>
           <Switch>
