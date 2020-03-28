3ac7a785eacd0be5a028d8f35c19101e36356208
Oana <oanamuntean8@gmail.com>
2020-03-28 17:27:03 +0200

    Add Useful Contacts content

--- src/components/UsefulContacts/index.js
+++ src/components/UsefulContacts/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Hero } from "@code4ro/taskforce-fe-components";
+import { Hero, SearchResultListItem } from "@code4ro/taskforce-fe-components";
+import contacts from "../../data/useful-contacts";
 
 import "./styles.scss";
 
@@ -7,6 +8,32 @@ const UsefulContacts = () => {
   return (
     <>
       <Hero title="Contacte utile" />
+      {contacts.map((contact, index) => (
+        <div key={index} className="contact-wrapper ">
+          <SearchResultListItem title={contact.name}>
+            <div className="contact-details">
+              <span>
+                <b>Website: </b>
+                <a
+                  href={contact.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {contact.website}
+                </a>
+              </span>
+              <span>
+                <b>Telefon:</b>
+                <a href={`tel:${contact.phone}`}>{contact.phone}</a>
+              </span>
+              <span>
+                <b>Email:</b>{" "}
+                <a href={`mailto:${contact.email}`}>{contact.email}</a>
+              </span>
+            </div>
+          </SearchResultListItem>
+        </div>
+      ))}
     </>
   );
 };
