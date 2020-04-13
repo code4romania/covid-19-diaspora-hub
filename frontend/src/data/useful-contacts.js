d719fbf24619dfe50da7911afea15ce3c8825387
Utwo <mihai.legat@gmail.com>
2020-04-13 16:27:57 +0300

    Update content for useful info and useful contacts

--- frontend/src/data/useful-contacts.js
+++ frontend/src/data/useful-contacts.js
@@ -1,17 +1,22 @@
 export default {
   title: "Contacte utile",
   content: `
-  <p style="background: #ffd738; padding: 10px; font-weight: bold">Pentru a fi la curent cu cele mai recente informatii despre regulile de călătorie si despre recomandările pentru românii <strong>din Europa</strong> intră pe <a target="_blank" rel="noopener noreferrer" href="http://www.mae.ro/node/51880">aceasta</a> pagina, selectează țara în care te afli și informează-te corect.</p>
-  <p style="background: #ffd738; padding: 10px; font-weight: bold">Pentru a fi la curent cu cele mai recente informatii despre regulile de călătorie si despre recomandările pentru românii din state <strong>din afara Europei</strong> intră pe <a target="_blank" rel="noopener noreferrer" href="http://www.mae.ro/node/51982">aceasta</a> pagina, selectează țara în care te afli și informează-te corect.</p>
-  <p>
-        Dacă sunteți în străinătate și doriți să aflați informații legate de
-        prevenirea infectării cu virusul COVID-19 (Coronavirus), puteți apela
+    <p>
+        Dacă vă aflați într-o situație dificilă, Ministerul Afacerilor Externe
+        vă recomandă să apelați numărul de telefon 
+        <a href="tel:+40214312059">+40-214-312059</a> sau să contactați misiunea
+        diplomatică / oficiul consular din statul în care vă aflați utilizând
+        datele de contact disponibile aici: <a
+          href="http://www.mae.ro/romanian-missions"
+          target="_blank"
+          rel="noopener noreferrer"
+        >mae.ro/romanian-missions</a>.
+    </p>
+    <p>
+        Dacă sunteți în străinătate și doriți să aflați <b>informații legate de
+        prevenirea infectării cu virusul COVID-19</b> (Coronavirus), puteți apela
         numărul de telefon: <a href="tel:+40213202020">+40-213-202020</a> din
-        orice rețea, cu tarif normal, 24 de ore din 24.
-      </p>
-      <p>
-        De asemenea puteti apela numărul de telefon <a href="tel:+40214312059">+40-214-312059</a> pentru notificarea
-        prezenței în statele în care se află la acest moment și comunicarea
-        datelor de contact.
+        orice rețea, cu tarif normal, 24 de ore din 24. Linia telefonică este 
+        destinată strict informării cetățenilor și nu este un număr de urgență.
       </p>`
 };
